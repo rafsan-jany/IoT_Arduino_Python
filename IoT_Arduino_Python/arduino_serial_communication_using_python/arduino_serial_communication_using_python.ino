c5a93a96962bf83291345c594263a0714a91c7cc
rafsan <rafsan.mbstu@gmail.com>
2019-10-22 01:13:31 +0600

    read from serial

--- IoT_Arduino_Python/arduino_serial_communication_using_python/arduino_serial_communication_using_python.ino
+++ IoT_Arduino_Python/arduino_serial_communication_using_python/arduino_serial_communication_using_python.ino
@@ -1,4 +1,8 @@
-String ser = "";
+String string_from_atmega2560 = "{\"D\":[1,2,3,4,5,6,7,8,9,10,11,12]}";
+
+String string_from_dragino;
+
+//int d = 0;
 
 void setup() {  
   Serial.begin(115200);  //From Ar9331  Black Window
@@ -7,19 +11,25 @@ void setup() {
   
 void loop() {  
   //From PC to Linino  
-  int c = Serial.read();              // read from USB-CDC  
+  
+  //int c = Serial.read();              // read from *OPENWRT* 
   //if (c != -1) {              
-    Serial1.print(c);            //        write char to UART  
-    //Serial1.write("Hi");
-    //c = c*10;
-    //Serial.print(c);
-    Serial.println(c);
+    //Serial1.print(c);            //        write char to  *ARDUINO_SERIAL*
+    if(Serial.available()){
+          string_from_dragino = Serial.readStringUntil("\n");
+          Serial1.println(string_from_dragino);
+    }
+    Serial.println(string_from_atmega2560); // print to *OPENWRT*
+    
   //}  
-  delay(1000);
+  delay(100);
+  //Serial.println("string_from_atmega2560");
   //From Linino to PC  
   //c = Serial1.read();                // read from UART  windows
+  //d = c * 10;
   //if (c != -1) {                      // got anything?  
     //Serial.write(c);                  //    write to USB-CDC  
+    //Serial.write(d);
     //Serial.write("Hello");
   //}  
 }
