218e20f0d706ea110dbb5559e9047cb446544e7e
rafsan <rafsan.mbstu@gmail.com>
2019-10-23 22:47:01 +0600

    added parsing for serial data read and function call

--- IoT_Arduino_Python/arduino_serial_communication_using_python/arduino_serial_communication_using_python.ino
+++ IoT_Arduino_Python/arduino_serial_communication_using_python/arduino_serial_communication_using_python.ino
@@ -1,35 +1,45 @@
 String string_from_atmega2560 = "{\"D\":[1,2,3,4,5,6,7,8,9,10,11,12]}";
-
 String string_from_dragino;
-
-//int d = 0;
+String method_type = "";
 
 void setup() {  
   Serial.begin(115200);  //From Ar9331  Black Window
   Serial1.begin(115200);  //To FTDI  Arduino Serial
 }  
   
-void loop() {  
-  //From PC to Linino  
-  
-  //int c = Serial.read();              // read from *OPENWRT* 
-  //if (c != -1) {              
-    //Serial1.print(c);            //        write char to  *ARDUINO_SERIAL*
+void loop() { 
     if(Serial.available()){
           string_from_dragino = Serial.readStringUntil("\n");
           Serial1.println(string_from_dragino);
     }
-    Serial.println(string_from_atmega2560); // print to *OPENWRT*
+
+    Serial.println(string_from_atmega2560);
     
-  //}  
-  delay(100);
-  //Serial.println("string_from_atmega2560");
-  //From Linino to PC  
-  //c = Serial1.read();                // read from UART  windows
-  //d = c * 10;
-  //if (c != -1) {                      // got anything?  
-    //Serial.write(c);                  //    write to USB-CDC  
-    //Serial.write(d);
-    //Serial.write("Hello");
-  //}  
+    int first_comma_index = string_from_dragino.indexOf(',');
+    int second_comma_index = string_from_dragino.indexOf(',',first_comma_index + 1);
+
+    String first_value_from_dragino = string_from_dragino.substring(0, first_comma_index);
+    String second_value_from_dragino = string_from_dragino.substring(first_comma_index + 1, second_comma_index);
+    String third_value_from_dragino = string_from_dragino.substring(second_comma_index + 1);
+
+    Serial1.println(first_value_from_dragino);
+    Serial1.println(second_value_from_dragino);
+    Serial1.println(third_value_from_dragino);
+
+    method_type = second_value_from_dragino;
+    
+    Serial.println(string_from_atmega2560); // print to *OPENWRT*
+    delay(1000);
+    
+    fun();
+
+    if (method_type == "digital") {
+      setRelayDat();
+    }
 }
+
+
+void fun(){
+  Serial.flush();
+  Serial.print("From fun()");
+  }
